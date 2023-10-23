822f525fe3a38752899dbfb4a7f4fad2515a64bd
Aleksander Pres <aleksander.pres98@gmail.com>
2023-10-23 13:29:15 +0200

    Enhance frontend

--- family_chronicle/chronicle/static/chronicle/home.js
+++ family_chronicle/chronicle/static/chronicle/home.js
@@ -2,6 +2,15 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('#posts').addEventListener('click', loadPostsView);
     document.querySelector('#albums').addEventListener('click', loadAlbumsView);
 
+    document.querySelector('#posts').addEventListener('mouseover', displayPostsOptions);
+    document.querySelector('#albums').addEventListener('mouseover', displayAlbumsOptions);
+
+    document.querySelector('#posts').addEventListener('mouseout', hidePostsOptions);
+    document.querySelector('#albums').addEventListener('mouseout', hideAlbumsOptions);
+
+    // Default load
+    hidePostsOptions();
+    hideAlbumsOptions();
     loadPostsView();
 });
 
@@ -14,4 +23,22 @@ function loadPostsView() {
 function loadAlbumsView() {
     document.querySelector("#posts-view").style.display = "none";
     document.querySelector("#albums-view").style.display = "block";
+}
+
+// Display dropdowns
+function displayPostsOptions() {
+    document.querySelector('#posts-opt').style.display = "block";
+}
+
+function displayAlbumsOptions() {
+    document.querySelector('#albums-opt').style.display = "block";
+}
+
+// Hide dropdowns
+function hidePostsOptions() {
+    document.querySelector('#posts-opt').style.display = "none";
+}
+
+function hideAlbumsOptions() {
+    document.querySelector('#albums-opt').style.display = "none";
 }
